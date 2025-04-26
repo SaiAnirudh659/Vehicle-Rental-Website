ccc1d191148891f43ea883d85d09cb9f16598439
Sai Anirudh Kasarla <saianirudhkasarla@Sais-MacBook-Air.local>
2025-04-26 14:02:33 -0400

    Added Home Page HTML skeleton with AngularJS setup

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('vehicleRentalApp', []);
+
+app.controller('MainController', function($scope) {
+    $scope.vehicleCategories = [
+        {
+            name: 'Luxury Cars',
+            description: 'Experience the best in comfort and style.',
+            image: 'images/luxury-car.jpg'
+        },
+        {
+            name: 'SUVs',
+            description: 'Spacious and powerful SUVs for every terrain.',
+            image: 'images/suv.jpg'
+        },
+        {
+            name: 'Motorbikes',
+            description: 'Speed through city or highways with top bikes.',
+            image: 'images/bike.jpg'
+        },
+        {
+            name: 'Electric Vehicles',
+            description: 'Go green and drive into the future.',
+            image: 'images/ev.jpg'
+        }
+    ];
+});
